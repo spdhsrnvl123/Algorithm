e0d46555a8688f40093811f8443c628a7919e2c8
Leutbing <spdhsrnvl123@naver.com>
2023-01-02 15:33:22 +0900

    Delete Programmers_Level1 directory

--- Programmers_Level1/서울에서_김서방_찾기.js
+++
@@ -1,8 +0,0 @@
-function solution(seoul) {
-    var answer = '';
-    
-    const location = seoul.findIndex((element) => element === "Kim")
-    answer += `김서방은 ${location}에 있다`
-    
-    return answer;
-}
--- Programmers_Level1/평균구하기.js
+++
@@ -1,9 +0,0 @@
-function solution(arr) {
-    var answer = 0;
-    let result = 0;
-    for(let i=0;i<arr.length;i++){
-        result += arr[i];
-        answer = result/arr.length
-    }
-    return answer;
-}
