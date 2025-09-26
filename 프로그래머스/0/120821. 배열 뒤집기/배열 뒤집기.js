165ccccc1c9e0624dfcc2cf4a7a5b159e8725d95
Goofipie <spdhsrnvl123@naver.com>
2025-09-26 14:08:39 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let answer = [];
+    // for(let i = num_list.length -1;i>=0; i--){
+    //     answer.push(num_list[i]);
+    // }
+    
+    answer = num_list.reverse();
+    
+    return answer;
+}
